0f270471fa467ab45c06e1f49afb968099d200c9
jahid727277-rgb <jahid727277@gmail.com>
2026-07-22 01:50:09 +0600

    Auto update

--- git-auto-sync.sh
+++ git-auto-sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m "Auto-update course materials and fixes $(date +'%Y-%m-%d %H:%M:%S')"
-echo "Changes committed successfully! Ready for GitHub sync."
+git commit -m "Auto-commit update: $(date +'%Y-%m-%d %H:%M:%S')"
+echo "Changes committed successfully!"
